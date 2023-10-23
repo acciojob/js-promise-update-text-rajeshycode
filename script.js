59295a87569cfe2c69e67c17413ae8e31a9010c5
rajeshycode <136836225+rajeshycode@users.noreply.github.com>
2023-10-23 21:01:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// script.js
+
+function delayHelloWorld() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Hello, world!");
+        }, 1000); // 1 second delay
+    });
+}
+
+delayHelloWorld().then((message) => {
+    const outputElement = document.getElementById("output");
+    outputElement.textContent = message;
+});
